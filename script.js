b13a27550d9268aabc90eb06531f82ae3c509ae6
altyw-nyw <altyshq001@gmail.com>
2025-03-29 19:34:50 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+var user = "Altyw" , age = "30", message = "Good day";
+console.log(user)
+console.log(age)
+console.log(message)
+
+
+var number = 20
+var string = "name"
+var b = 'city "Bishkek"'
+console.log(number)
+console.log(string)
+
+var a   //undefined
+console.log(b)
+console.log(a)
+
+var a = "меню"
+var b = 50
+console.log(a/b)
+
+
+
+var string = '10';
+console.log(string);
+console.log(Number(string));
+
+
+
+var a=20
+var b=5
+console.log(a>b)
+
+var a=20
+var b=5
+console.log(a===b)
+
+
+
+
+var a = prompt("Сумма")
+var coupon = prompt("Имеется купон?");
+console.log(coupon);
+
+var buy = 5000;
+var discountedBuy=buy;
+
+if (buy >= 5000) {
+    discountedBuy = buy - (buy / 100 * 10);
+    console.log(buy-(buy / 100 * 10))
+}
+if (buy >= 5000) {
+    discountedBuy = buy - (buy / 100 * 5);
+    console.log(buy-(buy / 100 * 10) - (buy / 100 * 5) )
+}
+
+var a = prompt("сумма")
+var coupon = confirm("Имеется купон?");
+console.log(coupon);
+
+
+
+
+
+
+
+
+
+
